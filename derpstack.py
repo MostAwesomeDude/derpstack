51c1c3be30998cafae9d4dfb1e15899941b0dcf8
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-11-08 10:57:37 -0800

    Synergize templating systems and HTML-based content.

--- derpstack.py
+++ derpstack.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask.ext.holster.main import init_holster
+from flask.ext.holster.simple import html
 
 app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return "It works!"
+init_holster(app)
+
+@app.holster("/")
+@html("index.html")
+def index():
+    return {"stories": ["Derp!"]}
+
+app.debug = True
